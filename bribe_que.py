b1468465fa3706be2a14c6db1f2de5dd890c50ad
Julia <julia@Julias-Air.lan>
2019-03-20 21:33:59 +0100

    hello here is the message

---
+++ bribe_que.py
@@ -0,0 +1,11 @@
+cue=[1,2,3,5,6,4]
+bribes=0
+for i in range(len(cue)):
+    bribes_i=0
+    for j in range(i,len(cue)):
+        if cue[i]>cue[j]:
+            bribes=bribes+1
+            bribes_i=bribes_i+1
+            if bribes_i>2:
+                print("Too chaotic")
+print(bribes)
